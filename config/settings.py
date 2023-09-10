2af39def2757bd575dc9b1ba1ffa89ebe2c0babd
web-weebie <twinklet969@gmail.com>
2023-09-10 09:07:22 +0100

    Chore: Modified .env file for allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -19,9 +19,8 @@ class GeneralSettings(BaseSettings):
     SECRET_KEY: str
     ALLOWED_HOSTS: List[str]
    
-
-
 GENERAL_SETTINGS = GeneralSettings()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
